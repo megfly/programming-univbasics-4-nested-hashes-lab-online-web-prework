7b230deb870348fd941d23b345217fea9afb5c5d
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-15 17:43:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,14 @@ def adding_matz
         }
      }
 
-    
+
+{
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+      :Languages => ["LISP", "C"]
+  }
+} 
+
 end
 
 def changing_alan
@@ -102,7 +113,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = ("GUI")
+
 end
 
 def adding_to_dennis
